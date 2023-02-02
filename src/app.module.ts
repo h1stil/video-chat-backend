b897ac447b1fb6e8b63e74fb8ff32c8d082c3dc7
Andrei <platonovaudit@yandex.ru>
2023-02-02 08:10:59 +0500

    feat: add env and postgres without data

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,23 @@ import { UsersController } from './modules/users/users.controller';
 import { UsersService } from './modules/users/users.service';
 import { UsersModule } from './modules/users/users.module';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
     UsersModule,
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'pgAdmin',
-      database: 'video-chat',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       models: [],
       autoLoadModels: true,
     }),
